17ee4f24eeec3136634dbd0b2c7d3ecae933e8ca
Neil Kalman <Neilkalman@gmail.com>
2018-11-18 23:54:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 const t = "are we cool with double quotes?"
 
+// test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
